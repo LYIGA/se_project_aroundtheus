49a931fcb81e46e8134edb8f514acbe20dd4b07b
SmithAkin <s29smit@gmail.com>
2023-03-26 21:12:07 -0500

    change to project 4

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,18 @@ const initialCards = [
         name: "Yosemite Valley", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
     },
     {
-        name: "Lake Louise", link: ""
+        name: "Lake Louise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
     }, 
     {
-        name: "Bald Mountains", link: ""
+        name: "Bald Mountains", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
     }, 
     {
-        name: "", link: ""
+        name: "Latemar", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
     }, 
     {
-        name: "", link: ""
+        name: "Vanoise National Park", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
     }, 
     {
-        name: "", link: ""
+        name: "Lago di Braies", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
     }
 ];
