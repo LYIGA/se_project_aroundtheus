dc9ff4ef26f0ff94a0e1e2f130316908598f9939
SmithAkin <s29smit@gmail.com>
2023-03-26 19:30:52 -0500

    change to project 4 stage 1

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const initialCards = [
+    {
+        name: "Yosemite Valley", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise", link: ""
+    }, 
+    {
+        name: "Bald Mountains", link: ""
+    }, 
+    {
+        name: "", link: ""
+    }, 
+    {
+        name: "", link: ""
+    }, 
+    {
+        name: "", link: ""
+    }
+];
