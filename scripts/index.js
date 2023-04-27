8169fd4e87bbb37293b5333e850cc4f5de6450e3
SmithAkin <s29smit@gmail.com>
2023-04-26 19:01:29 -0500

    change to final project 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,69 @@
 const initialCards = [
     {
-        name: "Yosemite Valley", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+        name: "Yosemite Valley",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
     },
     {
-        name: "Lake Louise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+        name: "Lake Louise",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
     }, 
     {
-        name: "Bald Mountains", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+        name: "Bald Mountains",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
     }, 
     {
-        name: "Latemar", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+        name: "Latemar",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
     }, 
     {
-        name: "Vanoise National Park", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+        name: "Vanoise National Park",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
     }, 
     {
-        name: "Lago di Braies", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-    }
+        name: "Lago di Braies",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    },
 ];
 
-const profileEditButton = document.querySelector('.profile__edit-button');
+/*--------------------------------------------------- */
+/*            Element                                 */
+/*--------------------------------------------------- */
+const profileEditButton = document.querySelector('#profile-edit-button');
+const profileCloseButton = document.querySelector('#modal-close-button');
 const profileEditModal = document.querySelector('#profile-edit-modal');
-const profileCloseButton = document.querySelector('#modal-close');
 const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
 const profileTitleInput = document.querySelector('#profile-title-input');
-const profileDescription = document.querySelector('#profile-description-input');
+const profileDescriptionInput = document.querySelector('#profile-description-input');
 
+const profileEditForm = profileEditModal.querySelector('.modal__form');
 
+/*----------------------------------------------------------- */
+/*                Function                                    */
+/*----------------------------------------------------------- */
+function closepopup() {
+    profileEditModal.classList.remove('modal_opened');
+}
+
+/*----------------------------------------------------------- */
+/*             Event Handelers                                */
+/*----------------------------------------------------------- */
+function handleProfileEditSubmit(e) {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closepopup();
+}
+
+/*----------------------------------------------------------- */
+/*                   Event Listeners
+/*----------------------------------------------------------- */
 profileEditButton.addEventListener('click', () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
     profileEditModal.classList.add('modal_opened');
-})
+});
+
+profileCloseButton.addEventListener('click', closepopup);
+
+profileEditForm.addEventListener('submit', handleProfileEditSubmit);
