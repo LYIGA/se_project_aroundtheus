3c410468d150e8713dd6b462fc9e9b1ca7c1600c
SmithAkin <s29smit@gmail.com>
2023-08-29 23:14:43 -0500

    refactor to FormVal project 7

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,7 +1,6 @@
 export class FormValidator {
   constructor(options, formEl) {
     this._formEl = formEl;
-    this._options = options;
     this._formSelector = options._formSelector;
     this._inputSelector = options._inputSelector;
     this._submitButtonSelector = options._submitButtonSelector;
@@ -10,23 +9,23 @@ export class FormValidator {
     this._errorClass = options._errorClass;
   }
   enableValidation() {
-    const formEls = [...document.querySelectorAll(this._options.formSelector)];
-    formEls.forEach((formEl) => {
-      formEl.addEventListener("submit", (e) => {
-        e.preventDefault();
-      });
-
-      setEventListener(formEl, options);
+    this._formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
     });
 
-    this._inputList = [...document.querySelectorAll(options._inputSelector)];
-    this._inputList.forEach((inputEl) => {
-      inputEl.addEventListener("input", () => {
-        this._checkValidity(inputEl);
+    this._setEventListeners();
+  }
+
+  _setEventListeners() {
+    const inputEls = [...formEl.querySelectorAll(this._inputSelector)];
+    const submitButton = formEl.querySelector(this._submitButtonSelector);
+    inputEls.forEach((inputEl) => {
+      inputEl.addEventListener("input", (e) => {
+        checkInputValidity(formEl, inputEl, options);
+        toggleButtonState(inputEls, submitButton, options);
       });
     });
   }
-
   _showInputError(inputEl) {
     const inputElId = `${inputEl.id}-error`;
     const errorEl = this._formEl.querySelector();
