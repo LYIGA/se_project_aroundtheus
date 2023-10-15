d463784c9c3d9ce2e08e36afbaaf94dea18275d3
SmithAkin <s29smit@gmail.com>
2023-10-15 16:44:41 -0500

    update

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,36 +1,37 @@
 export default class Popup {
-  constructor({ popupSelector }) {
+  constructor(popupSelector) {
     this._popupElement = document.querySelector(popupSelector);
+    this._open = this.open.bind(this);
+    this._close = this.close.bind(this);
+    this._handleEscClose = this.handleEscClose.bind(this);
   }
 
   open() {
     this._popupElement.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscClose);
   }
 
   close() {
     this._popupElement.classList.remove("modal_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
   }
 
-  _handleEscClose(evt) {
-    // if (evt.key === "Escape") {
-    console.log("i esc key");
-    // this.close();
-    // }
+  handleEscClose(e) {
+    if (e.key === "Escape") {
+      this.close();
+    }
   }
 
   setEventListeners() {
-    this._popupElement.addEventListener("mousedown", (evt) => {
-      if (evt.target.classList.contains("modal_opened")) {
-        this.close();
-      }
-
-      if (evt.target.classList.contains("modal__close")) {
+    this._popupElement.addEventListener("click", (e) => {
+      if (e.target.classList.contains("modal_opened")) {
         this.close();
       }
     });
-    this._handleEscClose();
-    // this._popupElement.addEventListener("keydown", (e) =>
-    //   this._handleEscClose(e)
-    // );
+    this._popupElement
+      .querySelector(".modal__close")
+      .addEventListener("click", () => {
+        this.close();
+      });
   }
 }
